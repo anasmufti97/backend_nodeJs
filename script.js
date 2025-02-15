f4b8cc0f0a1c9ef05aeb93e5b414daab1d4b45d8
ANAS MUFTI <muftianas97@gmail.com>
2025-02-15 13:32:25 +0500

    2nd

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
-});
+}
+)
+;
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+app.listen(3000, () => 
+    
+    {
+    
+        console.log('Server is running on port 3000');
+
+    }
+)
+;
