e75061e573594a10ba35838e815bb912fce08b1e
ANAS MUFTI <muftianas97@gmail.com>
2025-02-18 15:14:47 +0500

    code structured

--- userModel.js
+++ userModel.js
@@ -1,19 +1,12 @@
 const mongoose = require('mongoose');
 
-
 mongoose.connect('mongodb://localhost:27017/test');
 
 const userSchema = new mongoose.Schema({
-
-
     name: String,
     age: Number,
     email: String,
-
-
-
 });
-
 
 
 module.exports = mongoose.model('User', userSchema);
