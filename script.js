bf95211c742ff7db025e880cdca617df5c16bf18
ANAS MUFTI <muftianas97@gmail.com>
2025-02-18 15:12:29 +0500

    mongoose added

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
 const express = require('express');
 const app = express();
 
+const userModel = require('./userModel');
 
-app.get('/', (req, res) => {
-    res.send('Hello World! with nodemon');});
+
+
+app.use(function (req, res, next) {
+    console.log("Middle wareworking");
+    next();
+
+});
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World! with nodemon');
+// });
+
+// app.get('/:username', (req, res) => {
+//     res.send(req.params.username);
+
+// });
+
+
+app.get('/createUser', async (req, res) => {
+    let user = await userModel.create({
+        name: 'anas anas 2',
+        age: 25,
+        email: "anasmufti123@gmail.com"
+    })
+    res.send(user);
+}
+);
+
+app.get('/updateUser', async (req, res) => {
+    let user = await userModel.create({
+        name: 'anas anas 2',
+        age: 25,
+        email: "anasmufti123@gmail.com"
+    })
+    res.send(user);
+}
+);
+
+
+app.get('/read', async (req, res) => {
+    let user = await userModel.find();
+    res.send(user);
+}
+);
+
+
+
 
 
 app.listen(3000, () => {
